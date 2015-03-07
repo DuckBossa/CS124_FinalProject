5ca1648320777732d51ddbe4d01af33f4ab96025
Vermille Saw <veeann@users.noreply.github.com>
2015-03-07 17:04:03 +0800

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,10 @@
+//Item interface
+/*
+Very random class... 
+just so maging contributor ako at mag-show up siya sa profile ko
+and I don't have to search for the link to the rep again.. hahaha
+*/
+
+public interface Item {
+  public void effect();
+}
