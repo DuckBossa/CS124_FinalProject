14d7e429ad8c6f0bc06d1ef41d817b2bc66ce6ca
Vermille Saw <veeann@users.noreply.github.com>
2015-03-07 20:14:10 +0800

    Create Axe.java

---
+++ Axe.java
@@ -0,0 +1,20 @@
+//concrete Weapon Item
+
+public class Axe implements Item {
+	public boolean isWeapon () {
+		return true;
+	}
+	public boolean isArmor () {
+		return false;
+	}
+	public boolean isAccessory () {
+		return false;
+	}
+	public void use (Character c) {
+		c.attack += 10;
+	}
+	public void remove (Character c) {
+		c.attack -= 10;
+	}
+	public void activate (Character c) {}
+}
